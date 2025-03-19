0785800ee50305d3696ebb33cf3997bc956b1928
Magnet <phatthansn.charoenphol@gmail.com>
2025-03-19 11:34:11 +0700

    Change API Endpoint

--- src/libs/getVenue.tsx
+++ src/libs/getVenue.tsx
@@ -1,6 +1,6 @@
 export default async function getVenue(id:string) {
     await new Promise(resolve => setTimeout(resolve, 300));
-    const response = await fetch(`https://a08-venue-explorer-backend-3.vercel.app/api/v1/venues/${id}`)
+    const response = await fetch(`https://a08-venue-explorer-backend-2.vercel.app/api/v1/venues/${id}`)
     if(!response.ok){
         throw new Error("Fail to fetch venue")
     }
